defd7feee2a576864e743dfc551589d85884304a
Jude <Jude@192.168.2.4>
2017-05-28 21:41:38 +0530

    Commit again

---
+++ microservice-example-parent/microservice-service-dashboard/src/main/java/com/jba/dashboard/starter/TurbineDashboardApplication.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.jba.dashboard.starter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+/**
+ * @author Jude
+ * @since May 27, 2017
+ */
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableHystrixDashboard
+@EnableTurbine
+@SpringBootApplication
+public class TurbineDashboardApplication {
+	private static final Logger LOGGER = LoggerFactory.getLogger(TurbineDashboardApplication.class);
+
+	public static void main(String[] args) {
+		LOGGER.info("Dashboard Server is Starting");
+		SpringApplication.run(TurbineDashboardApplication.class, args);
+	}
+}
