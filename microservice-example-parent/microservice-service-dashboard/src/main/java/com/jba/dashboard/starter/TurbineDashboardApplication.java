d3954082c779bdc1f2401eab4f6361e3599d0f36
Jude <Jude@192.168.2.4>
2017-05-28 22:53:08 +0530

    Updated Commit

--- microservice-example-parent/microservice-service-dashboard/src/main/java/com/jba/dashboard/starter/TurbineDashboardApplication.java
+++ microservice-example-parent/microservice-service-dashboard/src/main/java/com/jba/dashboard/starter/TurbineDashboardApplication.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
@@ -16,7 +15,6 @@ import org.springframework.cloud.netflix.turbine.EnableTurbine;
  * @author Jude
  * @since May 27, 2017
  */
-@EnableEurekaClient
 @EnableDiscoveryClient
 @EnableHystrixDashboard
 @EnableTurbine
