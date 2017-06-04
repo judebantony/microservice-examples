0d12a7b33695688e019d31a65eb069c9c6612545
Jude <Jude@192.168.2.4>
2017-06-04 21:47:27 +0530

    Api Gateway Commit

---
+++ microservice-example-parent/mircoservice-gateway/src/main/java/com/jba/gateway/constant/GatewayConstants.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.jba.gateway.constant;
+
+/**
+ * @author Jude
+ * @since June 04, 2017
+ */
+public interface GatewayConstants {
+	public static final String WELCOME = "welcome";
+	public static final String WELCOME_URL = "/welcome/*";
+	public static final String ADMIN_URL = "/admin/*";
+	public static final String WELCOME_UI_URL = "/ui/welcome/*";
+	public static final String BACK_SLACH = "/";
+	public static final String PREFIX = "/WEB-INF/html/";
+	public static final String SUFFIX = ".html";
+	public static final String DEFAULT_INCLUDE_PATTERNS = "/api/*";
+	public static final String CSS = "/css/**";
+	public static final String JS = "/js/**";
+	public static final String USER ="user";
+	public static final String PWD ="password";
+	public static final String ROLE ="USER";
+	
+}
---
+++ microservice-example-parent/mircoservice-gateway/src/main/java/com/jba/gateway/starter/ApiGatewayApplication.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.jba.gateway.starter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author Jude
+ * @since June 04, 2017
+ */
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+@ComponentScan("com.jba")
+@EnableHystrix
+@EnableHystrixDashboard
+public class ApiGatewayApplication {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ApiGatewayApplication.class);
+
+	public static void main(String[] args) {
+		LOGGER.info("Api Gateway Server is Starting");
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
